572f5e8a3dbd871da4a705b54e7ffaae02a1581c
Patrick Gray <clifgray@Patricks-MacBook-Pro-2.local>
2016-03-27 20:08:24 -0400

    random changes

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Patrick Gray"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
